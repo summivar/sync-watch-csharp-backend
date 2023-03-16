1a305cd1c77f4588fcc485176f43b275c600c32e
vyacheslav <105442993+summivar@users.noreply.github.com>
2023-03-16 19:47:55 +0300

    Add Backend(init)

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService.Hubs
+{
+    public class FilmHub : Hub
+    {
+        private readonly IDictionary<string, UserConnection> _connections;
+        public FilmHub(IDictionary<string, UserConnection> connections)
+        {
+            _connections = connections;
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.GroupExcept(userConnection.Room, Context.ConnectionId)
+                    .SendAsync("ReceiveMessage", message);
+            }
+        }
+
+        public async Task PlayingVideo(bool play)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Groups(userConnection.Room)
+                    .SendAsync("PauseState", play);
+            }
+        }
+        public async Task RewindVideo(double seconds)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.GroupExcept(userConnection.Room, Context.ConnectionId)
+                    .SendAsync("TimeState", seconds);
+            }
+        }
+        public async Task ChangeVideo(string newURL)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Groups(userConnection.Room)
+                    .SendAsync("NewVideo", newURL);
+            }
+        }
+
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+            _connections[Context.ConnectionId] = userConnection;
+
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", $"{userConnection.User} has joined {userConnection.Room}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using ChatService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowCredentials()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddSingleton<IDictionary<string, UserConnection>>(options => new Dictionary<string, UserConnection>());
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseCors();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<FilmHub>("/hub");
+});
+
+app.Run();
